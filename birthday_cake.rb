13d9788a55f949c39983aba6df47b8353c216224
Laiba Afsar <150105364+laibaafsar7@users.noreply.github.com>
2024-03-23 10:48:04 -0500

    Update birthday_cake.rb

--- birthday_cake.rb
+++ birthday_cake.rb
@@ -1,1 +1,66 @@
+#correctedcode 
 
+Expected output:
+Happy 10th Birthday!
+Birthday cake with 10 blown out candles
+Lighting candles...
+Birthday cake with 10 lit candles
+Singing happy birthday...
+🎶Happy birthday to you, happy birthday to you🎶
+Blowing out candles...
+Birthday cake with 10 blown out candles
+
+#birthday_cake.rb
+
+require "active_support/all"
+
+class Birthdaycake
+  attr_accessor :age, :lit
+
+  def initialize(age)
+    self.age = age
+    self.lit = false
+  end
+
+  def candles_status
+    lit ? "lit" : "blown out"
+    end
+
+  def greet
+    "Happy #{self.age.ordinalize} Birthday!"
+  end
+
+  def sing
+    "🎶Happy birthday to you, happy birthday to you🎶"
+  end
+
+  def to_s
+    "Birthday cake with #{self.age} #{candles_status} candles"
+  end
+
+  def light_candles
+    self.lit = true
+  end
+
+  def blow_out_candles
+    self.lit = false
+  end
+
+  def self.celebrate(age)
+    birthday_cake = Birthdaycake.new(age)
+    puts birthday_cake.greet
+    puts birthday_cake.to_s
+    puts "Lighting candles..."
+    birthday_cake.light_candles
+    puts birthday_cake.to_s
+    puts "Singing happy birthday..."
+    puts birthday_cake.sing
+    puts "Blowing out candles..."
+    birthday_cake.blow_out_candles
+    puts birthday_cake.to_s
+
+    birthday_cake
+  end
+end
+
+Birthdaycake.celebrate(10)
